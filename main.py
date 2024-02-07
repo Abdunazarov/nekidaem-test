973a0a2f2457f7bd3ff6a7cc868cb811d01af8dd
Abdunazarov <abdunazarovdior@gmail.com>
2024-02-07 09:50:56 +0800

    Changed: news feed router

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 from fastapi import FastAPI
 
 # project
-from routers import blogs, users, subscriptions
+from routers import blogs, users, subscriptions, news_feed
 
 app = FastAPI()
 
 app.include_router(blogs.router)
 app.include_router(users.router)
 app.include_router(subscriptions.router)
+app.include_router(news_feed.router)
